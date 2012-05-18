7aa03431a8e3139bfb0c69a926046eba109579f7
Constantine Fedenko <cfedenko@gmail.com>
2012-05-18 10:51:40 +0300

    some minor changes & pep8

--- ddate.py
+++ ddate.py
@@ -1,28 +1,36 @@
-import datetime, calendar
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+import calendar
 
 DISCORDIAN_SEASONS = ["Chaos", "Discord", "Confusion", "Bureaucracy",
     "The Aftermath"]
 
-DISCORDIAN_WEEKDAYS=["Sweetmorn", "Boomtime", "Pungenday", "Prickle-Prickle",
+DISCORDIAN_WEEKDAYS = ["Sweetmorn", "Boomtime", "Pungenday", "Prickle-Prickle",
     "Setting Orange"]
 
+
 def ddate(year, month, day):
+    """
+        stolen from otfbot (http://www.otfbot.org/)
+    """
     today = datetime.date(year, month, day)
     is_leap_year = calendar.isleap(year)
     if is_leap_year and month == 2 and day == 29:
         return "St. Tib's Day, YOLD " + (year + 1166)
-  
+
     day_of_year = today.timetuple().tm_yday - 1
-  
-    weekday=day_of_year % 5
+
+    weekday = day_of_year % 5
     if is_leap_year and day_of_year >= 60:
-        day_of_year -= 1 # Compensate for St. Tib's Day
-  
+        day_of_year -= 1  # Compensate for St. Tib's Day
+
     season, dday = divmod(day_of_year, 73)
     return "Today is %s, the %d day of %s in the YOLD %d" \
         % (DISCORDIAN_WEEKDAYS[weekday], dday + 1, \
         DISCORDIAN_SEASONS[season], year + 1166)
 
-
-today = datetime.date.today()
-print ddate(today.year, today.month, today.day)
+if __name__ == '__main__':
+    today = datetime.date.today()
+    print ddate(today.year, today.month, today.day)
