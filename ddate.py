70e173a3bc2776656c0fa466b73fc617914a8d0f
Konstantin Fedenko <cfedenko@gmail.com>
2012-06-07 12:16:49 +0300

    added argparse

--- ddate.py
+++ ddate.py
@@ -3,6 +3,8 @@
 
 import datetime
 import calendar
+import argparse
+import sys
 
 DISCORDIAN_SEASONS = ["Chaos", "Discord", "Confusion", "Bureaucracy",
     "The Aftermath"]
@@ -22,15 +24,34 @@ def ddate(year, month, day):
 
     day_of_year = today.timetuple().tm_yday - 1
 
-    weekday = day_of_year % 5
+
     if is_leap_year and day_of_year >= 60:
         day_of_year -= 1  # Compensate for St. Tib's Day
 
+    weekday = day_of_year % 5
+
     season, dday = divmod(day_of_year, 73)
-    return "Today is %s, the %d day of %s in the YOLD %d" \
-        % (DISCORDIAN_WEEKDAYS[weekday], dday + 1, \
-        DISCORDIAN_SEASONS[season], year + 1166)
+    return "%s, %s %d, %d YOLD" \
+        % (DISCORDIAN_WEEKDAYS[weekday],\
+        DISCORDIAN_SEASONS[season], dday + 1, year + 1166)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('day', type=int, nargs='?')
+    parser.add_argument('month', type=int, nargs='?')
+    parser.add_argument('year', type=int, nargs='?')
+    args = parser.parse_args()
+
+    if not(bool(args.day) == bool(args.month) == bool(args.year)):
+        parser.error('too few arguments')
+
+    if args.day:
+        message = ddate(args.year, args.month, args.day)
+    else:
+        today = datetime.date(year, month, day)
+        message = ddate(today.year, today.month, today.day)
+
+    sys.stdout.write("%s\n" % message)
 
 if __name__ == '__main__':
-    today = datetime.date.today()
-    print ddate(today.year, today.month, today.day)
+    main()
